6d822f0d79d5f825f7e842872d6d3f50935dce49
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-10-22 12:43:35 -0300

    Built

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -1,8 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject cube;
+    public Material trigger_Mat;
+    public Material trigger_Mat2;
+
     void Start()
     {
         
@@ -12,5 +16,27 @@ public class TriggerScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            cube.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        cube.SetActive(true);
+        gameObject.GetComponent<Renderer>().material = trigger_Mat;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Renderer>().material = trigger_Mat2;
+        }
     }
 }
